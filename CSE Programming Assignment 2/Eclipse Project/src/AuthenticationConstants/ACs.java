52851e2751c648c9f3aca83507114e56b7c82977
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2017-04-18 16:27:16 +0800

    Changed authentication constants class

--- CSE Programming Assignment 2/Eclipse Project/src/AuthenticationConstants/ACs.java
+++ CSE Programming Assignment 2/Eclipse Project/src/AuthenticationConstants/ACs.java
@@ -7,8 +7,11 @@ public final class ACs {
 	public static final String REQUESTSIGNEDCERT = "Give me your signed Certificate";
 	public static final String TERMINATEMSG = "Bye";
 	public static final String CLIENTID = "I am client";
+	public static final String CLIENTPUBLICKEY = "Client Public key";
 	
 	// Server
 	public static final String SERVERID = "I am SecStore";
 	public static final String SIGNEDCERT = "1234567890";
+	public static final String REQUESTCLIENTPUBLICKEY = "Send me your public key";	
+	
 }
--- CSE Programming Assignment 2/Eclipse Project/src/Server/ServerClass.java
+++ CSE Programming Assignment 2/Eclipse Project/src/Server/ServerClass.java
@@ -10,47 +10,78 @@ import AuthenticationConstants.ACs;	// Authentication Constants
 
 public class ServerClass {
 	
+	// Client
+	public static final String AUTHENTICATIONMSG = "Hello SecStore, prove your ID";
+	public static final String REQUESTSIGNEDCERT = "Give me your signed Certificate";
+	public static final String TERMINATEMSG = "Bye";
+	public static final String CLIENTID = "I am client";
+	public static final String CLIENTPUBLICKEY = "Client Public key";
+	
+	// Server
+	public static final String SERVERID = "I am SecStore";
+	public static final String SIGNEDCERT = "1234567890";
+	public static final String REQUESTCLIENTPUBLICKEY = "Send me your public key";	
+	
+	
+	private static boolean terminateConnection(PrintWriter out){
+		out.println(ACs.TERMINATEMSG);
+		return false;
+	}
+	
 	private static boolean authenticationProtocol(BufferedReader in, PrintWriter out) throws IOException{
 		
+		System.out.println("Starting authentication protocol");
+		
 		if(!(in.readLine()).equals(ACs.AUTHENTICATIONMSG)){
-			out.println(ACs.TERMINATEMSG);
-			return false;
+			terminateConnection(out);
 		}
 		out.println(ACs.SERVERID);
 		if(!(in.readLine().equals(ACs.REQUESTSIGNEDCERT))){
-			out.println(ACs.TERMINATEMSG);
-			return false;
+			terminateConnection(out);
 		}
 		out.println(ACs.SIGNEDCERT);
 		
-		if(!(in.readLine().equals(ACs.CLIENTID))){
-			out.println(ACs.TERMINATEMSG);
-			return false;
-		}
+		String message = in.readLine();	// Reads in the clientsID using client's private key
 		
-		out.println();
+		out.println(ACs.REQUESTCLIENTPUBLICKEY);
+		
+		if(!(in.readLine().equals(ACs.CLIENTPUBLICKEY))){
+			terminateConnection(out);
+		}
 		
 		return false;
 	}
 	
 	public static void main(String[] args) throws IOException{
 		
+		/*
+		 * Possible IPs in school : 
+		 * 			202.94.70.51
+		 * 			103.24.77.51
+		 * 
+		 * 
+		 */
+		
 		//String hostName = args[0];
 		//int portNum = Integer.parseInt(args[1]);
+		
 		int portNum = 7777;	// socket address
 		String hostName = "NicG";
 		ServerSocket serverSocket;
 		Socket clientSocket;
 		
 		serverSocket = new ServerSocket(portNum);
+		
+		System.out.println("Accepting client connections now ...");
 		clientSocket = serverSocket.accept();
+		System.out.println("Client connection established!");
 		
 		BufferedReader in = new BufferedReader(
 								new InputStreamReader(
 										clientSocket.getInputStream()));
 		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 		
-		
+		boolean proceed = authenticationProtocol(in,out);
 	
 		
 		
