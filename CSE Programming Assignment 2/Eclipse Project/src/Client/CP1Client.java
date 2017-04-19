0a2c120fbcbb64606fdcb0b9bf4edfd1c702e925
Yin Ji Sheng <G@Gs-MacBook-Pro.dhcp.sutd.edu.sg>
2017-04-19 16:43:23 +0800

    Client appears to be working for CP1

--- CSE Programming Assignment 2/Eclipse Project/src/Client/CP1Client.java
+++ CSE Programming Assignment 2/Eclipse Project/src/Client/CP1Client.java
@@ -1,5 +1,10 @@
+package Client;
+
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.File;
 
 import AuthenticationConstants.ACs;
 import java.io.FileInputStream;
@@ -17,13 +22,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Base64;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -33,7 +44,7 @@ import javax.xml.bind.DatatypeConverter;
 
 public class CP1Client {
 
-	public static void main(String[] args) throws IOException, InvalidKeyException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+	public static void main(String[] args) throws Exception {
 		System.out.println("trying to connect");
 		String hostName = "10.12.21.29";
 		int portNumber = 7777;
@@ -42,110 +53,187 @@ public class CP1Client {
 		echoSocket.connect(sockaddr, 8080);
 		System.out.println("connected");
 		PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-		//DataInputStream inData = new DataInputStream((echoSocket.getInputStream()));
-		//DataInputStream in = new DataInputStream(new InputStreamReader(echoSocket.getInputStream()));
 		BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-		//InputStream in = echoSocket.getInputStream();
-		//OutputStream out = echoSocket.getOutputStream();
 		
-		//send message and receive encrypted message
+		//send nonce as the message for the server to encrypt, to make sure no playback attack can take place
+		byte[] nonce = new byte[32];
+        Random rand;
+        rand = SecureRandom.getInstance ("SHA1PRNG");
+        rand.nextBytes(nonce);
+        String nonceString = new String(nonce, "UTF-16");
+        // Send over nonce
+        System.out.println("sending over nonce");
+        out.println(DatatypeConverter.printBase64Binary(nonce));
+        out.flush();
+		
+		
+		//REPLACED BY NONCE send message and receive encrypted message
+        /*
 		String initialMessage = ACs.AUTHENTICATIONMSG;
 		out.println(initialMessage);
 		out.flush();
+		*/
+		//String initialSize = in.readLine();
+        
+        //receive encrypted nonce from server
 		String serverInitialReply = in.readLine();
-		System.out.println("gave me secret message");
+		System.out.println("gave me secret message: " + serverInitialReply);
 		
 		//send request for cert and receive signed cert
 		String secondMessage = ACs.REQUESTSIGNEDCERT;
 		out.println(secondMessage);
 		out.flush();
 		String sizeInString = in.readLine();
+		
 		int certificateSize = Integer.parseInt(sizeInString);
 		byte[] signedCertificate = new byte[certificateSize];
-		signedCertificate = DatatypeConverter.parseBase64Binary(in.readLine());
+		String signedCertificateInString = in.readLine();
+		signedCertificate = DatatypeConverter.parseBase64Binary(signedCertificateInString);
 		System.out.println("gave me signed certificate");
 		
 		//extract public key from signed certificate
 		//creating X509 certificate object
 		FileOutputStream fileOutput = new FileOutputStream("CA.crt");
-        fileOutput.write(signedCertificate, 0, certificateSize);
+		fileOutput.write(signedCertificate, 0, signedCertificate.length);
         FileInputStream certFileInput = new FileInputStream("CA.crt");
-         
-		CertificateFactory cf = CertificateFactory.getInstance("X.509");
-		X509Certificate CAcert = (X509Certificate)cf.generateCertificate(certFileInput);
+
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509Certificate CAcert = (X509Certificate) cf.generateCertificate(certFileInput);
 						
 		//extract public key from the certificate 
 		PublicKey CAkey = CAcert.getPublicKey();				
 		CAcert.checkValidity();
-		CAcert.verify(CAkey);
+		System.out.println("public key of CA extracted");
+
 		
-		//use public key to decrypt serverInitialReply
+		//use public key to decrypt signed certificate to extract public key of server
 		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 		cipher.init(Cipher.DECRYPT_MODE, CAkey);
 		byte[] decryptedBytes = cipher.doFinal(DatatypeConverter.parseBase64Binary(serverInitialReply));
-		String decryptedMessage = DatatypeConverter.printBase64Binary(decryptedBytes);
-		System.out.println("message decrypted" + decryptedMessage);
-		
+		String decryptedMessage = new String (decryptedBytes, "UTF-16");
+        System.out.println("decryptedMessage: " + decryptedMessage);
+        
 		//if serverInitialReply is correct, then proceed to give my encrypted client ID
-		if (!decryptedMessage.equals(ACs.SERVERID)){
+		if (!decryptedMessage.equals(nonceString)){
 			out.println(ACs.TERMINATEMSG);
+			out.flush();
+			out.close();
+			in.close();
+			echoSocket.close();
+			System.out.println("authentication failed");
+			return;
+		} 
+		System.out.println("successfully authenticated the server");
+		
+		//generate keypair here
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(1024);
+		KeyPair keyPair = keyGen.generateKeyPair();
+		Key publicKey = keyPair.getPublic();
+		Key privateKey = keyPair.getPrivate();
+		
+		//receive nonce from server
+		byte[] serverNonceInBytes = new byte[32];
+		String serverNonce = in.readLine();
+		serverNonceInBytes = DatatypeConverter.parseBase64Binary(serverNonce);
+		System.out.println("received nonce from server: " + serverNonce);
+		
+		//encrypt nonce using client private key and send it back to server
+		Cipher Ecipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		Ecipher.init(Cipher.ENCRYPT_MODE, privateKey);
+		byte[] encryptedServerNonce = Ecipher.doFinal(serverNonceInBytes);
+		out.println(DatatypeConverter.printBase64Binary(encryptedServerNonce));
+		out.flush();	
+		System.out.println("sent encrypted nonce to server");
+
+		
+		//wait for server to ask for public key, send public key to server
+		String requestForPublic = in.readLine();
+		if (!requestForPublic.equals(ACs.REQUESTCLIENTPUBLICKEY)){
+			out.println("you didn't ask for the public key");
+			out.flush();
+			out.close();
+			in.close();
+			echoSocket.close();
+			System.out.println("failed to request public key");
+			return;
+		}
+		
+
+		String encodedKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+		out.println(encodedKey);
+		out.flush();
+		System.out.println("sent public key to server");
+		
+		//receive success message and initialise handshake
+		String successMessage = in.readLine();
+		if (!successMessage.equals(ACs.SERVERREADYTORECEIVE)){
+			out.println("you didn't tell me you're ready to receive my files");
+			out.flush();
 			out.close();
 			in.close();
 			echoSocket.close();
 			return;
-		} 
-		System.out.println("successfully authenticated the server");
-		//encrypt clientId 
+		}
 		
-		//get public key 
-		Path keyPath = Paths.get("publicServer.der");
-		byte[] publicKeyInBytes = Files.readAllBytes(keyPath);
+		System.out.println("initialising handshake");
 		
-		out.println(ACs.CLIENTID);
-		out.flush();
+		//use server's public key to encrypt the clients files and send it back to server
+		for (int i = 0; i < args.length; i++){
+			//tell server this is the starting time
+			File fileToBeSent = new File(args[i]);
+			byte[] fileBytes = new byte[(int)fileToBeSent.length()];
+			BufferedInputStream fileInput = new BufferedInputStream(new FileInputStream(fileToBeSent));
+			fileInput.read(fileBytes,0,fileBytes.length);
+			fileInput.close();
+			
+			//encrypt this file
+			Cipher Ecipher2 = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			Ecipher2.init(Cipher.ENCRYPT_MODE, CAkey);
+			byte[] encryptedFile = encryptFile(fileBytes, Ecipher2);
+			out.println(args[i]);
+			out.println(Integer.toString(encryptedFile.length));
+			//String encryptedFileInString = new String(encryptedFile, "UTF-16");
+			out.println(DatatypeConverter.printBase64Binary(encryptedFile));
+			System.out.println("successfully sent over " + args[i]);
+		}
+		out.println(ACs.CLIENTDONE);
+		System.out.println("told server all ecnrypted files are sent");
 		
-		
-		/*
-		if (!signedCertificate.equals(ACs.SIGNEDCERT) || !serverInitialReply.equals(ACs.SERVERID)){
-			out.println(ACs.TERMINATEMSG);
+		//wait for server to reply to be done
+		String serverReceiveMessage = in.readLine();
+		if (!serverReceiveMessage.equals(ACs.SERVERRECEIVED)){
+			out.println("you didn't tell me you received my files");
 			out.flush();
+			out.close();
+			in.close();
+			echoSocket.close();
 			return;
-		} else {
-			out.println(ACs.CLIENTID);
-			out.flush();
 		}
-		System.out.println("decryption of secret message is complete");
-		*/
+		System.out.println("Done!");
 		
-		
-		
-		//proceed to send my public key
-		//int serverThirdMessage = in.read();
-		/*
-		if (!serverThirdMessage.equals(ACs.REQUESTCLIENTPUBLICKEY)){
-			out.print(ACs.TERMINATEMSG);
-			out.flush();	
-		} else {
-			out.println(ACs.CLIENTPUBLICKEY);
-			out.flush();
-		}
-		
-		System.out.println("sent my public key");
-		*/
-		
-		
-		
-		//if pass, then do handshake to send files
-		//int serverFourthMessage = in.read();
-		/*
-		if (serverFourthMessage.equals(ACs.SERVERREADYTORECEIVE)){
-			//proceed
-			System.out.println("everything works, proceed to shake hand");
-		} else {
-			System.out.println("didnt work");
-		}
-		*/
 	}
+	
+	public static byte[] encryptFile(byte[] fileBytes, Cipher rsaECipher) throws Exception{
+	      ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
 
+	      int start = 0;
+	      int fileLength = fileBytes.length;
+	      while (start < fileLength) {
+	          byte[] tempBuff;
+	          if (fileLength - start >= 117) {
+	              tempBuff = rsaECipher.doFinal(fileBytes, start, 117);
+	              //System.out.println(Arrays.toString(tempBuff));
+	          } else {
+	              tempBuff = rsaECipher.doFinal(fileBytes, start, fileLength - start);
+	          }
+	          byteOutput.write(tempBuff, 0, tempBuff.length);
+	          start += 117;
+	      }
+	      byte[] encryptedFileBytes = byteOutput.toByteArray();
+	      byteOutput.close();
+	      return encryptedFileBytes;
+
+	    }
 
 }
